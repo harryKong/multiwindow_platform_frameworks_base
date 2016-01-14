fd94dfb20ebe42d89453d6ad4d55bdd7e067d51d
hx <hx@ph.com>
2016-01-14 12:13:17 +0800

    add pm subcmd preinstall

--- cmds/pm/src/com/android/commands/pm/Pm.java
+++ cmds/pm/src/com/android/commands/pm/Pm.java
@@ -117,6 +117,11 @@ public final class Pm {
             return;
         }
 
+        if ("preinstall".equals(op)) {
+            runPreInstall();
+            return;
+        }
+
         if ("uninstall".equals(op)) {
             runUninstall();
             return;
@@ -976,6 +981,51 @@ public final class Pm {
             System.err.println(e.toString());
             System.err.println(PM_NOT_RUNNING_ERR);
         }
+    }
+
+    private void runPreInstall() {
+         String path = nextArg();
+         int i;
+
+         System.err.println("\t preInstall path: " + path);
+         if (path == null) {
+            System.err.println("Error: no package specified");
+            showUsage();
+            return;
+        }
+
+        File[] files = new File(path).listFiles();
+
+        for(File apkFilePath : files) {
+            System.err.println("\tpkg: " + apkFilePath);
+            PackageInstallObserver obs = new PackageInstallObserver();          
+            try {
+                mPm.installPackage(Uri.fromFile(apkFilePath), obs, 0,null);
+                System.err.println("\t pkg----1------: ");
+                synchronized (obs) {
+                    while (!obs.finished) {
+                        try {
+                            System.err.println("\t pkg----2------: ");
+                            obs.wait();
+                            System.err.println("\t pkg----3------: ");
+                        } catch (InterruptedException e) {
+                            System.err.println("\t pkg----4------: ");
+                        }
+                    }
+                    if (obs.result == PackageManager.INSTALL_SUCCEEDED) {
+                        System.out.println("Success");
+                    } else {
+                        System.err.println("Failure ["
+                            + installFailureToString(obs.result)
+                            + "]");
+                    }
+                }
+            } catch (RemoteException e) {
+                System.err.println(e.toString());
+                System.err.println(PM_NOT_RUNNING_ERR);
+            }
+        }
+        System.err.println("\t preInstall path: " + path + " ok");  
     }
 
     /**
